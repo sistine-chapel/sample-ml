203dc5f761bb1537e347341294139f0a39142d56
ALI <alikeshavarzbh@gmail.com>
2020-11-30 22:17:16 +0330

    fix: formatting

--- feature_crosses.py
+++ feature_crosses.py
@@ -1,4 +1,4 @@
-#@title Load the imports
+# @title Load the imports
 
 # from __future__ import absolute_import, division, print_function, unicode_literals
 
@@ -25,7 +25,7 @@ test_df = pd.read_csv("./data/california_housing_test.csv")
 # Scale the labels
 scale_factor = 1000.0
 # Scale the training set's label.
-train_df["median_house_value"] /= scale_factor 
+train_df["median_house_value"] /= scale_factor
 
 # Scale the test set's label
 test_df["median_house_value"] /= scale_factor
@@ -48,55 +48,58 @@ feature_columns.append(longitude)
 # part of the model. Understanding layers is not important right now.
 fp_feature_layer = layers.DenseFeatures(feature_columns)
 
-#@title Define functions to create and train a model, and a plotting function
+# @title Define functions to create and train a model, and a plotting function
+
+
 def create_model(my_learning_rate, feature_layer):
-  """Create and compile a simple linear regression model."""
-  # Most simple tf.keras models are sequential.
-  model = tf.keras.models.Sequential()
+    """Create and compile a simple linear regression model."""
+    # Most simple tf.keras models are sequential.
+    model = tf.keras.models.Sequential()
 
-  # Add the layer containing the feature columns to the model.
-  model.add(feature_layer)
+    # Add the layer containing the feature columns to the model.
+    model.add(feature_layer)
 
-  # Add one linear layer to the model to yield a simple linear regressor.
-  model.add(tf.keras.layers.Dense(units=1, input_shape=(1,)))
+    # Add one linear layer to the model to yield a simple linear regressor.
+    model.add(tf.keras.layers.Dense(units=1, input_shape=(1,)))
 
-  # Construct the layers into a model that TensorFlow can execute.
-  model.compile(optimizer=tf.keras.optimizers.RMSprop(lr=my_learning_rate),
-                loss="mean_squared_error",
-                metrics=[tf.keras.metrics.RootMeanSquaredError()])
+    # Construct the layers into a model that TensorFlow can execute.
+    model.compile(optimizer=tf.keras.optimizers.RMSprop(lr=my_learning_rate),
+                  loss="mean_squared_error",
+                  metrics=[tf.keras.metrics.RootMeanSquaredError()])
 
-  return model           
+    return model
 
 
 def train_model(model, dataset, epochs, batch_size, label_name):
-  """Feed a dataset into the model in order to train it."""
+    """Feed a dataset into the model in order to train it."""
 
-  features = {name:np.array(value) for name, value in dataset.items()}
-  label = np.array(features.pop(label_name))
-  history = model.fit(x=features, y=label, batch_size=batch_size,
-                      epochs=epochs, shuffle=True)
+    features = {name: np.array(value) for name, value in dataset.items()}
+    label = np.array(features.pop(label_name))
+    history = model.fit(x=features, y=label, batch_size=batch_size,
+                        epochs=epochs, shuffle=True)
 
-  # The list of epochs is stored separately from the rest of history.
-  epochs = history.epoch
-  
-  # Isolate the mean absolute error for each epoch.
-  hist = pd.DataFrame(history.history)
-  rmse = hist["root_mean_squared_error"]
+    # The list of epochs is stored separately from the rest of history.
+    epochs = history.epoch
 
-  return epochs, rmse   
+    # Isolate the mean absolute error for each epoch.
+    hist = pd.DataFrame(history.history)
+    rmse = hist["root_mean_squared_error"]
+
+    return epochs, rmse
 
 
 def plot_the_loss_curve(epochs, rmse):
-  """Plot a curve of loss vs. epoch."""
+    """Plot a curve of loss vs. epoch."""
 
-  plt.figure()
-  plt.xlabel("Epoch")
-  plt.ylabel("Root Mean Squared Error")
+    plt.figure()
+    plt.xlabel("Epoch")
+    plt.ylabel("Root Mean Squared Error")
 
-  plt.plot(epochs, rmse, label="Loss")
-  plt.legend()
-  plt.ylim([rmse.min()*0.94, rmse.max()* 1.05])
-  plt.show()  
+    plt.plot(epochs, rmse, label="Loss")
+    plt.legend()
+    plt.ylim([rmse.min()*0.94, rmse.max() * 1.05])
+    plt.show()
+
 
 print("Defined the create_model, train_model, and plot_the_loss_curve functions.")
 
@@ -115,30 +118,30 @@ epochs, rmse = train_model(my_model, train_df, epochs, batch_size, label_name)
 plot_the_loss_curve(epochs, rmse)
 
 print("\n: Evaluate the new model against the test set:")
-test_features = {name:np.array(value) for name, value in test_df.items()}
+test_features = {name: np.array(value) for name, value in test_df.items()}
 test_label = np.array(test_features.pop(label_name))
 my_model.evaluate(x=test_features, y=test_label, batch_size=batch_size)
 
-resolution_in_degrees = 1.0 
+resolution_in_degrees = 1.0
 
 # Create a new empty list that will eventually hold the generated feature column.
 feature_columns = []
 
 # Create a bucket feature column for latitude.
 latitude_as_a_numeric_column = tf.feature_column.numeric_column("latitude")
-latitude_boundaries = list(np.arange(int(min(train_df['latitude'])), 
-                                     int(max(train_df['latitude'])), 
+latitude_boundaries = list(np.arange(int(min(train_df['latitude'])),
+                                     int(max(train_df['latitude'])),
                                      resolution_in_degrees))
-latitude = tf.feature_column.bucketized_column(latitude_as_a_numeric_column, 
+latitude = tf.feature_column.bucketized_column(latitude_as_a_numeric_column,
                                                latitude_boundaries)
 feature_columns.append(latitude)
 
 # Create a bucket feature column for longitude.
 longitude_as_a_numeric_column = tf.feature_column.numeric_column("longitude")
-longitude_boundaries = list(np.arange(int(min(train_df['longitude'])), 
-                                      int(max(train_df['longitude'])), 
+longitude_boundaries = list(np.arange(int(min(train_df['longitude'])),
+                                      int(max(train_df['longitude'])),
                                       resolution_in_degrees))
-longitude = tf.feature_column.bucketized_column(longitude_as_a_numeric_column, 
+longitude = tf.feature_column.bucketized_column(longitude_as_a_numeric_column,
                                                 longitude_boundaries)
 feature_columns.append(longitude)
 
@@ -161,35 +164,40 @@ plot_the_loss_curve(epochs, rmse)
 print("\n: Evaluate the new model against the test set:")
 my_model.evaluate(x=test_features, y=test_label, batch_size=batch_size)
 
-resolution_in_degrees = 1.0 
+resolution_in_degrees = 1.0
 
 # Create a new empty list that will eventually hold the generated feature column.
 feature_columns = []
 
 # Create a bucket feature column for latitude.
 latitude_as_a_numeric_column = tf.feature_column.numeric_column("latitude")
-latitude_boundaries = list(np.arange(int(min(train_df['latitude'])), int(max(train_df['latitude'])), resolution_in_degrees))
-latitude = tf.feature_column.bucketized_column(latitude_as_a_numeric_column, latitude_boundaries)
+latitude_boundaries = list(np.arange(int(min(train_df['latitude'])), int(
+    max(train_df['latitude'])), resolution_in_degrees))
+latitude = tf.feature_column.bucketized_column(
+    latitude_as_a_numeric_column, latitude_boundaries)
 
 # Create a bucket feature column for longitude.
 longitude_as_a_numeric_column = tf.feature_column.numeric_column("longitude")
-longitude_boundaries = list(np.arange(int(min(train_df['longitude'])), int(max(train_df['longitude'])), resolution_in_degrees))
-longitude = tf.feature_column.bucketized_column(longitude_as_a_numeric_column, longitude_boundaries)
+longitude_boundaries = list(np.arange(int(min(train_df['longitude'])), int(
+    max(train_df['longitude'])), resolution_in_degrees))
+longitude = tf.feature_column.bucketized_column(
+    longitude_as_a_numeric_column, longitude_boundaries)
 
 # Create a feature cross of latitude and longitude.
-latitude_x_longitude = tf.feature_column.crossed_column([latitude, longitude], hash_bucket_size=100)
+latitude_x_longitude = tf.feature_column.crossed_column(
+    [latitude, longitude], hash_bucket_size=100)
 crossed_feature = tf.feature_column.indicator_column(latitude_x_longitude)
 feature_columns.append(crossed_feature)
 
 # Convert the list of feature columns into a layer that will later be fed into
-# the model. 
+# the model.
 feature_cross_feature_layer = layers.DenseFeatures(feature_columns)
 
 # The following variables are the hyperparameters.
 learning_rate = 0.04
 epochs = 35
 
-# Build the model, this time passing in the feature_cross_feature_layer: 
+# Build the model, this time passing in the feature_cross_feature_layer:
 my_model = create_model(learning_rate, feature_cross_feature_layer)
 
 # Train the model on the training set.
@@ -199,4 +207,3 @@ plot_the_loss_curve(epochs, rmse)
 
 print("\n: Evaluate the new model against the test set:")
 my_model.evaluate(x=test_features, y=test_label, batch_size=batch_size)
-
